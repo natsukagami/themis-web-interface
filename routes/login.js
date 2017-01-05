5f5c126dad898930f9ec94cf9778cc316b022bc8
Natsu Kagami <natsukagami@gmail.com>
2017-01-05 08:29:01 +0700

    Closes #3

--- routes/login.js
+++ routes/login.js
@@ -4,13 +4,14 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.render('login', {
-		title: 'Đăng nhập'
+		title: 'Đăng nhập',
+		failed: req.query.failed === 'true'
 	});
 });
 
 router.post('/', passport.authenticate('local', {
 	successRedirect: '/',
-	failureRedirect: '/login'
+	failureRedirect: '/login?failed=true'
 }));
 
 module.exports = router;
