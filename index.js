8deba7fc5c5b53fbd870b5eaf1d494bb23dfcb5a
Natsu Kagami <natsukagami@gmail.com>
2017-05-26 21:21:54 +0700

    Fixed a bug where version is shown with 2 'v'

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 try {
 	const version = fs.readFileSync('twi.version', 'utf-8');
 	app.use((req, res, next) => {
-		res.locals.TWIVersion = 'v' + version;
+		res.locals.TWIVersion = version;
 		next();
 	});
 } catch (e) {
